59b4b5cdcb3768bf83a4c010168be38481f088d0
Pranav Manole <44233934+pranavmanole26@users.noreply.github.com>
2022-09-29 14:42:38 +0530

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -31,9 +31,13 @@ resource "launchdarkly_feature_flag" "ratelimit-config-ff" {
 
     variation_type = "json"
     variations {
-        name = "dev"
         value = jsonencode({
             "GET_/test" : {}
         })
     }
+    variations {
+          value = jsonencode({
+              "GET_/test1" : {}
+          })
+      }
 }
